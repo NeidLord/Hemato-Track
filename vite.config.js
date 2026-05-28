1204cf9d2d597339af813f4b36b1ddc018d7dd82
Sebastian Gallardo <gallardosebas8668@gmail.com>
2026-05-28 08:18:03 -0400

    instalacion de plugin para usar como app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'], 
+      manifest: {
+        name: 'Sistema Hemotransf Pro',
+        short_name: 'Hemotransf',
+        description: 'Sistema integral de trazabilidad para Bancos de Sangre',
+        theme_color: '#1e3a8a', 
+        background_color: '#f8fafc', 
+        display: 'standalone', 
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
